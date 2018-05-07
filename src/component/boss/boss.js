d66b2c7a4af73581abd592cc9426fff7c80c3d79
Wei Wei Zhang <WWEZ@NNE.COM>
2018-05-07 11:54:23 +0800

    change

--- src/component/boss/boss.js
+++ src/component/boss/boss.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import {Card,WingBlank,WhiteSpace} from 'antd-mobile';
+import {connect} from 'react-redux';
+import {getUserList} from '../../redux/chatuser.redux';
+
+@connect(
+    state=>state.chatuser,
+    {getUserList}
+)
 class Boss extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            data:[]
-        }
-    }
     componentDidMount(){
-        axios.get(`/user/list?type=employee`)
-             .then(res=>{
-                 if(res.data.code===0){
-                     this.setState({
-                         data:res.data.data
-                     })
-                 }
-             })
+        this.props.getUserList(`employee`);
     }
     render(){
         const Header=Card.Header;
@@ -24,7 +18,7 @@ class Boss extends React.Component{
         return (
             <WingBlank>
                 <WhiteSpace/>
-             {this.state.data.map(v=>(
+             {this.props.userList.map(v=>(
                 v.avatar ? (<Card key={v._id}>
                      <Header 
                         title={v.user}
